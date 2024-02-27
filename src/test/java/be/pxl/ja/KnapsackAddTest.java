6bdafdf94686f78d6ca137cb28715d292f872e17
Nele Custers <custersnele@gmail.com>
2024-02-27 10:53:26 +0100

    update tests

--- src/test/java/be/pxl/ja/KnapsackAddTest.java
+++ src/test/java/be/pxl/ja/KnapsackAddTest.java
@@ -1,7 +1,15 @@
 package be.pxl.ja;
 
+import be.pxl.ja.knapsack.Knapsack;
+import be.pxl.ja.knapsack.KnapsackFullException;
 import be.pxl.ja.robbery.Product;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 public class KnapsackAddTest {
@@ -13,29 +21,29 @@ public class KnapsackAddTest {
 		product = new Product("Something", 10, 12);
 	}
 
-//	@Test
-//	public void itemIsAddedWhenThereIsEnoughCapacity() throws KnapsackFullException {
-//		Knapsack<Product> knapsack = new Knapsack<>(20);
-//		knapsack.add(product);
-//
-//		assertTrue(knapsack.getItems().contains(product));
-//	}
-//
-//	@Test
-//	public void whenItemExceedsMaximumCapacityKnapsackFullExceptionIsThrown() {
-//		Knapsack<Product> knapsack = new Knapsack<>(20);
-//		product.setWeight(25);
-//		assertThrows(KnapsackFullException.class, () -> knapsack.add(product));
-//		assertFalse(knapsack.getItems().contains(product));
-//	}
-//
-//	@Test
-//	public void whenAddingExtraItemExceedsMaximumCapacityKnapsackFullExceptionIsThrown() throws KnapsackFullException {
-//		Knapsack<Product> knapsack = new Knapsack<>(20);
-//		knapsack.add(new Product("Product 1", 5, 12));
-//		knapsack.add(new Product("Product 2", 6, 15));
-//
-//		assertThrows(KnapsackFullException.class, () -> knapsack.add(product));
-//		assertFalse(knapsack.getItems().contains(product));
-//	}
+	@Test
+	public void itemIsAddedWhenThereIsEnoughCapacity() throws KnapsackFullException {
+		Knapsack<Product> knapsack = new Knapsack<>(20);
+		knapsack.add(product);
+
+		assertTrue(knapsack.getItems().contains(product));
+	}
+
+	@Test
+	public void whenItemExceedsMaximumCapacityKnapsackFullExceptionIsThrown() {
+		Knapsack<Product> knapsack = new Knapsack<>(20);
+		product.setWeight(25);
+		assertThrows(KnapsackFullException.class, () -> knapsack.add(product));
+		assertFalse(knapsack.getItems().contains(product));
+	}
+
+	@Test
+	public void whenAddingExtraItemExceedsMaximumCapacityKnapsackFullExceptionIsThrown() throws KnapsackFullException {
+		Knapsack<Product> knapsack = new Knapsack<>(20);
+		knapsack.add(new Product("Product 1", 5, 12));
+		knapsack.add(new Product("Product 2", 6, 15));
+
+		assertThrows(KnapsackFullException.class, () -> knapsack.add(product));
+		assertFalse(knapsack.getItems().contains(product));
+	}
 }
--- src/test/java/be/pxl/ja/KnapsackGetCurrentWeightTest.java
+++ src/test/java/be/pxl/ja/KnapsackGetCurrentWeightTest.java
@@ -1,6 +1,10 @@
 package be.pxl.ja;
 
+import be.pxl.ja.knapsack.Knapsack;
+import be.pxl.ja.knapsack.KnapsackFullException;
 import be.pxl.ja.robbery.Product;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -8,33 +12,33 @@ public class KnapsackGetCurrentWeightTest {
 
 	private Product product;
 
-//	@BeforeEach
-//	public void init() {
-//		product = new Product("Something", 10, 12);
-//	}
-//
-//	@Test
-//	public void emptyKnapsackHasWeightZero() {
-//		Knapsack<Product> knapsack = new Knapsack<>(20);
-//
-//		assertEquals(0, knapsack.getCurrentWeight());
-//	}
-//
-//	@Test
-//	public void knapsackWithOneItemReturnsWeightOfTheItem() throws KnapsackFullException {
-//		Knapsack<Product> knapsack = new Knapsack<>(20);
-//		knapsack.add(product);
-//
-//		assertEquals(10, knapsack.getCurrentWeight());
-//	}
-//
-//	@Test
-//	public void knapsackWithMultipleItemsReturnsSumOfWeights() throws KnapsackFullException {
-//		Knapsack<Product> knapsack = new Knapsack<>(20);
-//		knapsack.add(new Product("Product 1", 5, 12));
-//		knapsack.add(new Product("Product 2", 6, 15));
-//		knapsack.add(new Product("Product 3", 7, 22));
-//
-//		assertEquals(18, knapsack.getCurrentWeight());
-//	}
+	@BeforeEach
+	public void init() {
+		product = new Product("Something", 10, 12);
+	}
+
+	@Test
+	public void emptyKnapsackHasWeightZero() {
+		Knapsack<Product> knapsack = new Knapsack<>(20);
+
+		assertEquals(0, knapsack.getCurrentWeight());
+	}
+
+	@Test
+	public void knapsackWithOneItemReturnsWeightOfTheItem() throws KnapsackFullException {
+		Knapsack<Product> knapsack = new Knapsack<>(20);
+		knapsack.add(product);
+
+		assertEquals(10, knapsack.getCurrentWeight());
+	}
+
+	@Test
+	public void knapsackWithMultipleItemsReturnsSumOfWeights() throws KnapsackFullException {
+		Knapsack<Product> knapsack = new Knapsack<>(20);
+		knapsack.add(new Product("Product 1", 5, 12));
+		knapsack.add(new Product("Product 2", 6, 15));
+		knapsack.add(new Product("Product 3", 7, 22));
+
+		assertEquals(18, knapsack.getCurrentWeight());
+	}
 }
